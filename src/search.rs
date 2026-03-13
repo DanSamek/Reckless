cea467194ccff68d425943acee1f769d0fcdf063
Daniel Samek <samekda3@fit.cvut.cz>
2026-03-13 11:54:33 +0100

    take 2 - adjust division constant (256 -> 128)
    bench 3241389

--- src/search.rs
+++ src/search.rs
@@ -534,7 +534,7 @@ fn search<NODE: NodeType>(
         && estimated_score >= eval
         && eval
             >= beta - 9 * depth + 126 * tt_pv as i32 - 128 * improvement / 1024 + 286
-                - 20 * (td.stack[ply + 1].cutoff_count < 2) as i32 - nmp_history_value / 256
+                - 20 * (td.stack[ply + 1].cutoff_count < 2) as i32 - nmp_history_value / 128
         && ply as i32 >= td.nmp_min_ply
         && td.board.has_non_pawns()
         && !is_loss(beta)
